70c1db9c6b31148f29d6158b0e4b8de5642b7fa6
Kruiiz <42588014+Kruiiz@users.noreply.github.com>
2019-05-19 00:35:10 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function preload () {
+  img1 =loadImage("instaworst.png");
+  img2 =loadImage("IMNOTSAD.jpg");
+  img3 =loadImage("IMNOTSAD.jpg");
+  img4 =loadImage("IMNOTSAD.jpg");
+  img5 =loadImage("IMNOTSAD.jpg");
+}
+function setup() {
+  createCanvas(800, 800);
+  noStroke();
+  fill(0);
+  
+}
+
+function draw() {
+  noTint();
+  background(img1);
+  if ((mouseX <= 400) && (mouseY <= 400)) {
+    tint('pink');
+    image(img2, 0, 0);   // Upper-left
+  }
+  else if ((mouseX <= 400) && (mouseY > 400)) {              tint('orange');
+    image(img3, 0, 400);  // Lower-left
+  }
+  else if ((mouseX > 400) && (mouseY <= 400)) {
+    tint('lightgreen');
+    image(img4, 400, 0);  // Upper-right
+  }
+  else {
+    tint('magenta');
+    image(img5, 400, 400); // Lower-right
+  }
+}
