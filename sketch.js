38723d173ea0ce769b9f41a1611c85a25d0dd6c6
Kruiiz <42588014+Kruiiz@users.noreply.github.com>
2019-05-20 01:00:35 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,33 @@
-function preload(){
-  img1 = loadImage("heart1.png");
-  img2 = loadImage("Addictions.png");
+function preload () {
+  img1 =loadImage("instaworst.png");
+  img2 =loadImage("IMNOTSAD.jpg");
+  img3 =loadImage("IMNOTSAD.jpg");
+  img4 =loadImage("IMNOTSAD.jpg");
+  img5 =loadImage("IMNOTSAD.jpg");
 }
 function setup() {
-  createCanvas(600, 800);
-fill(0, 102);
-  background(img2); // Draw once to give a little color
+  createCanvas(800, 800);
+  noStroke();
+  fill(0);
+  
 }
+
 function draw() {
-} // Empty draw() keeps the program running
-function mousePressed() {
-  let c = color('lightpink');
-  fill(c);
-  imageMode(CENTER);
-  image( img1, mouseX, mouseY);
+  noTint();
+  background(img1);
+  if ((mouseX <= 400) && (mouseY <= 400)) {
+    tint('pink');
+    image(img2, 0, 0);   // Upper-left
+  }
+  else if ((mouseX <= 400) && (mouseY > 400)) {              tint('orange');
+    image(img3, 0, 400);  // Lower-left
+  }
+  else if ((mouseX > 400) && (mouseY <= 400)) {
+    tint('lightgreen');
+    image(img4, 400, 0);  // Upper-right
+  }
+  else {
+    tint('magenta');
+    image(img5, 400, 400); // Lower-right
+  }
 }
